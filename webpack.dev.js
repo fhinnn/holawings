71bb5fc8d0623e5bc7269cfa85eeaa3f2f0c53ff
Dhafin Almas Nusantara <88534235+fhinnn@users.noreply.github.com>
2025-07-14 22:49:21 +0700

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,6 +1,12 @@
 const { merge } = require('webpack-merge');
 const common = require('./webpack.common');
- 
+
 module.exports = merge(common, {
-  mode: 'development'
-})
+  mode: 'development',
+  devServer: {
+    allowedHosts: 'all', // penting untuk menghindari Invalid Host header
+    host: '0.0.0.0',      // biar bisa diakses dari luar container
+    port: 8080,           // wajib: Cloud Run pakai port 8080
+    hot: true             // opsional: aktifkan hot reloading
+  }
+});
